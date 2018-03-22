35ddf48078ba6f4fe9e2f6adab150cef54b704e9
Gabriel Karczewski <>
2018-03-22 22:12:51 +0100

    created App - main React component

---
+++ resources/js/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <section>
+          <div className="container">
+            <h1>iUpload</h1>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default App
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import App from './components/App.js'
+
 ReactDOM.render(
-  <h1>iUpload</h1>,
+  <App />,
   document.getElementById('app')
 )
