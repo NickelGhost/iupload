f7ed28236d69dc8e29e52fadbfb6ff760205dfd4
Gabriel Karczewski <>
2018-03-22 18:05:01 +0100

    removed an unnecessary semicolon

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello there!')
 })
 
-const port = 3000;
+const port = 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
