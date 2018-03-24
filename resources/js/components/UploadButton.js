237a8713ed6d59cda3c6d4ae539ce3a14d29cbec
Gabriel Karczewski <nickelghost@tuta.io>
2018-03-24 23:05:35 +0100

    added the file list to the modal

--- resources/js/components/UploadButton.js
+++ resources/js/components/UploadButton.js
@@ -9,7 +9,8 @@ class UploadButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      files: []
     };
 
     this.toggle = this.toggle.bind(this);
@@ -21,6 +22,12 @@ class UploadButton extends React.Component {
     });
   }
 
+  onDrop(files) {
+    this.setState({
+      files
+    });
+  }
+
   render() {
     return (
       <div>
@@ -28,13 +35,18 @@ class UploadButton extends React.Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle}>
           <ModalHeader toggle={this.toggle}>Upload Photos</ModalHeader>
           <ModalBody>
-            <Dropzone className="dropzone">
+            <Dropzone className="dropzone mb-4" onDrop={this.onDrop.bind(this)}>
               <p className="text-center">Click to select a file, or drop one here</p>
             </Dropzone>
+            <aside>
+              <h3>Dropped files</h3>
+              <ul className="photo-list">
+                {
+                  this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+                }
+              </ul>
+            </aside>
           </ModalBody>
-          <ModalFooter>
-            <Button className="choose-photo" color="success" onClick={this.toggle}>Upload</Button>
-          </ModalFooter>
         </Modal>
       </div>
     );
