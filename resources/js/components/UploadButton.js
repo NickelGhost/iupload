86f4f465d36a2371a7dc2e6208c6250042bb4173
Gabriel Karczewski <nickelghost@tuta.io>
2018-03-24 23:25:26 +0100

    added disabling of the dropzone when files are being uploaded

--- resources/js/components/UploadButton.js
+++ resources/js/components/UploadButton.js
@@ -10,6 +10,7 @@ class UploadButton extends React.Component {
     super(props)
     this.state = {
       modal: false,
+      dropzone: true,
       files: []
     }
 
@@ -24,19 +25,25 @@ class UploadButton extends React.Component {
 
   onDrop(files) {
     this.setState({
-      files
+      files,
+      dropzone: false
     })
   }
 
   render() {
+    const dropzone = this.state.dropzone
+    const dropzoneText = dropzone ?
+      'Click to select a file, or drop one here' :
+      'Please wait while the files are being uploaded'
+
     return (
       <div>
         <Button className="choose-photo" color="success" onClick={this.toggle}>Choose a photo...</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle}>
           <ModalHeader toggle={this.toggle}>Upload Photos</ModalHeader>
           <ModalBody>
-            <Dropzone className="dropzone mb-4" onDrop={this.onDrop.bind(this)}>
-              <p className="text-center">Click to select a file, or drop one here</p>
+            <Dropzone className="dropzone mb-4" onDrop={this.onDrop.bind(this)} disabled={!dropzone}>
+              <p className="text-center">{dropzoneText}</p>
             </Dropzone>
             <aside>
               <h3>Dropped files</h3>
