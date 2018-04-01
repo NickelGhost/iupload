66587b4258a6b738372b11722c92784904eef1ed
Gabriel Karczewski <nickelghost@tuta.io>
2018-04-01 19:47:19 +0100

    moved the port constant to other constants

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 const publicDir = `${__dirname}/public`
+const port = 3000
 
 app.use(express.static(publicDir))
 
@@ -9,7 +10,6 @@ app.get('*', function (req, res) {
   res.sendFile(`${publicDir}/index.html`)
 })
 
-const port = 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
