2cdc2011f6eb9f43ea419b236d3591744e1f708f
Gabriel Karczewski <nickelghost@tuta.io>
2018-06-10 01:26:40 +0100

    fixed props issue

--- resources/js/components/DroppedFileListItem.jsx
+++ resources/js/components/DroppedFileListItem.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 
 import { Button } from 'reactstrap';
 
-function DroppedFileListItem() {
-  const f = this.props.file;
+function DroppedFileListItem({ file }) {
   return (
     <li>
       <div className="item mb-3">
         <div className="row no-gutters">
           <div className="col-3">
-            <img className="img" src={f.thumbnail} alt="user upload" />
+            <img className="img" src={file.thumbnail} alt="user upload" />
           </div>
           <div className="col name">
-            {f.name}
+            {file.name}
           </div>
         </div>
         <div className="row options">
           <div className="col">
             <div className="px-3 py-1 float-right">
-              <Button color="info" href={`/${f.name}`}>View</Button>
+              <Button color="info" href={`/${file.name}`}>View</Button>
             </div>
           </div>
         </div>
