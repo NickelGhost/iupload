356802b22b03645e4bad84ea155d47cdc07e7f5a
Gabriel Karczewski <nickelghost@tuta.io>
2018-06-10 01:38:50 +0100

    revised most of index.js

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
-const express = require('express')
-const app = express()
-const fs = require('fs')
-const fileUpload = require('express-fileupload')
-const cors = require('cors')
-const gm = require('gm')
+const express = require('express');
 
-app.use(cors())
-app.use(fileUpload())
+const app = express();
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const gm = require('gm');
 
-const publicDir = `${__dirname}/public`
-const port = process.env.PORT || 3000
+app.use(cors());
+app.use(fileUpload());
 
-app.use(express.static(publicDir))
+const publicDir = `${__dirname}/public`;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicDir));
 
 app.get('/api/images', (req, res) => {
   fs.readdir(`${publicDir}/upload/thumbnails`, (err, files) => {
-    let images = []
-    files.splice(0, 1) //removes .gitkeep
-    for (const file of files) {
-      images.unshift({
-        url: `/upload/thumbnails/${file}`,
-        name: file
-      })
-    }
-    return res.json(images)
-  })
-})
+    files.splice(0, 1); // removes .gitkeep
+    const images = files.map(file => ({
+      url: `/upload/thumbnails/${file}`,
+      name: file,
+    }));
+    return res.json(images);
+  });
+});
 
 app.get('/api/images/:id', (req, res) => {
-  const path = `${publicDir}/upload/${req.params.id}`
+  const path = `${publicDir}/upload/${req.params.id}`;
   if (fs.existsSync(path)) {
-    const url = `/upload/${req.params.id}`
+    const url = `/upload/${req.params.id}`;
     return res.json({
       name: req.params.id,
-      url
-    })
-  } else {
-    return res.status(404).send('not found')
+      url,
+    });
   }
-})
+  return res.status(404).send('not found');
+});
 
 app.post('/api/images/upload', (req, res) => {
-  const file = req.files.file
-  const extension = file.mimetype.split('/')[1]
+  const { file } = req.files;
+  const extension = file.mimetype.split('/')[1];
   let num, name, path, thumbnailPath
 
   do {
@@ -70,9 +67,9 @@ app.post('/api/images/upload', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(`${publicDir}/index.html`)
-})
+  res.sendFile(`${publicDir}/index.html`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-})
+  console.log(`Example app listening on port ${port}!`);
+});
