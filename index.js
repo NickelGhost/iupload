96ad9a348d33a8ae8a54e3727d32757dd2998cd5
Gabriel Karczewski <nickelghost@outlook.com>
2020-02-10 22:10:41 +0100

    Make code more readible with async/await

--- index.js
+++ index.js
@@ -1,73 +1,86 @@
-const express = require('express');
-
-const app = express();
+const util = require('util');
 const fs = require('fs');
+const { resolve } = require('path');
+
+const express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const gm = require('gm');
 
-app.use(cors());
-app.use(fileUpload());
 
-const publicDir = `${__dirname}/public`;
+gm.prototype.write = util.promisify(gm.prototype.write);
+fs.readdir = util.promisify(fs.readdir);
+fs.exists = util.promisify(fs.exists);
+
+const publicDir = resolve(__dirname, 'public');
 const port = process.env.PORT || 3000;
 
+const app = express();
+
+app.use(cors());
+app.use(fileUpload());
 app.use(express.static(publicDir));
 
-app.get('/api/images', (req, res) => {
-  fs.readdir(`${publicDir}/upload/thumbnails`, (err, files) => {
-    files.splice(0, 1); // removes .gitkeep
-    const images = files.map(file => ({
-      url: `/upload/thumbnails/${file}`,
-      name: file,
-    }));
-    return res.json(images);
-  });
+app.get('/api/images', async (req, res) => {
+  let files;
+  try {
+    files = await fs.readdir(`${publicDir}/upload/thumbnails`);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+  files.splice(0, 1); // removes .gitkeep
+  const images = files.map(file => ({
+    url: `/upload/thumbnails/${file}`,
+    name: file,
+  }));
+  return res.json(images);
 });
 
 app.get('/api/images/:id', (req, res) => {
   const path = `${publicDir}/upload/${req.params.id}`;
-  if (fs.existsSync(path)) {
-    const url = `/upload/${req.params.id}`;
-    return res.json({
-      name: req.params.id,
-      url,
-    });
+  if (!fs.existsSync(path)) {
+    return res.status(404).send('not found');
   }
-  return res.status(404).send('not found');
+  const url = `/upload/${req.params.id}`;
+  return res.json({
+    name: req.params.id,
+    url,
+  });
 });
 
-app.post('/api/images/upload', (req, res) => {
+app.post('/api/images/upload', async (req, res) => {
   const { file } = req.files;
   const extension = file.mimetype.split('/')[1];
   let num;
   let name;
   let path;
   let thumbnailPath;
-
+  // If the generated filename exists, do again
   do {
     num = Math.random().toString(36).substring(7);
     name = `${num}.${extension}`;
     path = `${publicDir}/upload/${name}`;
     thumbnailPath = `${publicDir}/upload/thumbnails/${name}`;
   } while (fs.existsSync(path));
-
-  file.mv(path, (mvErr) => {
-    if (mvErr) {
-      return res.status(500).send(mvErr);
-    }
-    gm(path)
+  // Allow for using await
+  file.mv = util.promisify(file.mv);
+  // Move the file to our specified path
+  try {
+    await file.mv(path);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+  // Create a thumbnail
+  try {
+    await gm(path)
       .resizeExact(250, 250, '!')
-      .write(thumbnailPath, (writeErr) => {
-        if (writeErr) {
-          fs.unlinkSync(path);
-          return res.status(500).send(writeErr);
-        }
-        const thumbnail = `/upload/thumbnails/${name}`;
-        return res.json({ name, thumbnail });
-      });
-    return true;
-  });
+      .write(thumbnailPath);
+  } catch (err) {
+    fs.unlinkSync(path);
+    return res.status(500).send(err);
+  }
+  const thumbnail = `/upload/thumbnails/${name}`;
+  return res.json({ name, thumbnail });
 });
 
 app.get('*', (req, res) => {
@@ -75,5 +88,6 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}!`);
 });
